65b10c0ec3a3f8a09c29449c5dc395978c1b6d18
rlindsberg <roderick@karlemstrand.com>
2021-05-03 21:29:41 +0200

    Add all layers

--- model.py
+++ model.py
@@ -10,9 +10,20 @@ class ResNet18(nn.Module):
         self.in_channels = 64
         self.conv_1 = nn.Conv2d(3, 64, kernel_size=3)
         self.batch_norm_1 = nn.BatchNorm2d(64)
+
         self.residual_block_1 = self.add_block_layer(out_channels=64,
                                                      n_layers=layers_in_each_block_list[0],
                                                      stride=1)
+        self.residual_block_2 = self.add_block_layer(out_channels=128,
+                                                     n_layers=layers_in_each_block_list[1],
+                                                     stride=1)
+        self.residual_block_3 = self.add_block_layer(out_channels=256,
+                                                     n_layers=layers_in_each_block_list[2],
+                                                     stride=1)
+        self.residual_block_4 = self.add_block_layer(out_channels=512,
+                                                     n_layers=layers_in_each_block_list[3],
+                                                     stride=1)
+        self.output = nn.Linear(in_features=512, out_features=10)
 
     def add_block_layer(self, n_layers, stride, out_channels):
         # stride_for_each_layer_list = [1,1]
