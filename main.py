4df7d37b33d414b6ef42fbb884ff176011c35dcb
Roderick Karlemstrand <roderick@karlemstrand.com>
2021-05-05 21:27:18 +0200

    Resnet18 train function + CE loss (#2)
    * Implement training function and test
    
    * Seperate load data into a function. return loss for training
    
    * Return loss in test
    
    * Implement plot
    
    * Use all data

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 # main file for training
-import os
-import torch
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
 from torch.utils.data import Dataset, DataLoader
+import torchvision
 from torchvision import transforms, utils
+import torch.optim as optim
+from tqdm import tqdm
+
 from Custom_dataset import CDONdataset
-from conv_block import ConvBlock
 from model import ResNet18
 
+# set global env variable
+if torch.cuda.is_available():
+    print('GPU is enabled!')
+    device = 'cuda'
+else:
+    print('No GPU!')
+    device = 'cpu'
 
-def load_dataset():
+
+def load_cdon_dataset():
 
     # Data
-    print('==> Preparing data..')
+    print('==> Preparing CDON data..')
 
     transform = transforms.Compose([
             transforms.ToPILImage(),
@@ -44,21 +54,132 @@ def load_dataset():
     return train_loader
 
 
+def train(model, criterion, optimizer, train_loader):
+    loss_batch = []
+
+    # activate train mode
+    model.train()
+
+    for batch_idx, (inputs, targets) in enumerate(train_loader):
+        # to(device) copies data from CPU to GPU
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        # loss is a Tensor, therefore:
+        loss_batch.append(loss.item())
+
+    return np.sum(loss_batch)
+
+
+def test(model, criterion, test_loader):
+    loss_batch = []
+
+    # activate eval mode
+    model.eval()
+
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(test_loader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            loss_batch.append(loss.item())
+
+            # outputs is 100x10 (batch_size x n_classes)
+            # max value, axis=1
+            # returns max value of each column and the index
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+    acc = correct / total
+    loss = np.sum(loss_batch)
+    return loss, acc
+
+
+def load_cifar10_dataset():
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    train_data = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=128, shuffle=True, num_workers=2)
+
+    test_data = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=100, shuffle=False, num_workers=2)
+
+    return train_loader, test_loader
+
+
+def plot_learning_curve_and_acc(train_cost, test_cost, test_acc):
+    # plot learning curve
+    plt.plot(train_cost)
+    plt.plot(test_cost)
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend(['Train', 'Test'])
+    plt.show()
+
+    plt.plot(test_acc)
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.show()
+
+
 def main():
-    train_loader = load_dataset()
-    x = None
-    for train_images, train_labels in train_loader:
-        print(train_images.shape)
-        print(train_labels)
-        x = train_images
+    # Temp commented for CIFAR-10
+    # train_loader = load_cdon_dataset()
+    # x = None
+    # for train_images, train_labels in train_loader:
+    #     print(train_images.shape)
+    #     print(train_labels)
+    #     x = train_images
 
     layers_in_each_block_list = [2, 2, 2, 2]
-    model = ResNet18(layers_in_each_block_list)
+    model = ResNet18(layers_in_each_block_list).to(device)
 
-    # classify 2 images
-    out = model.forward(x)
-    print(out.shape)
-    pass
+    """Prepare data"""
+    # classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    print('==> Preparing data..')
+    train_loader, test_loader = load_cifar10_dataset()
+
+    """training for 10 epochs"""
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200, eta_min=0.001)
+
+    loss_train_plot = []
+    loss_test_plot = []
+    acc_test_plot = []
+    for _ in tqdm(range(3)):
+        loss_train = train(model, criterion, optimizer, train_loader)
+        loss_train_plot.append(loss_train)
+
+        loss_test, acc_test = test(model, criterion, test_loader)
+        loss_test_plot.append(loss_test)
+        acc_test_plot.append(acc_test)
+
+        # anneal learning rate
+        scheduler.step()
+
+    """Plot learning curve and accuracy"""
+    plot_learning_curve_and_acc(loss_train_plot, loss_test_plot, acc_test_plot)
 
 
 if __name__ == '__main__':
