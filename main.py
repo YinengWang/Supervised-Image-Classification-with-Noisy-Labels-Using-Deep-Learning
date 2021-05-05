e2fd7c341dd24732d05f8daf4b3bf9d41c6efe4e
rlindsberg <roderick@karlemstrand.com>
2021-05-05 18:23:03 +0200

    Use all data

--- main.py
+++ main.py
@@ -119,7 +119,6 @@ def load_cifar10_dataset():
 
     train_data = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
 
-    train_data.data = train_data.data[:10000]
     train_loader = torch.utils.data.DataLoader(train_data, batch_size=128, shuffle=True, num_workers=2)
 
     test_data = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
