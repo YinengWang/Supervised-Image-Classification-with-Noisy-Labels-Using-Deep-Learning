1b4d1cfd11adb384ec7b4d5357c222dc8478545a
Alessio Galatolo <galatoloalessio@gmail.com>
2021-05-06 13:25:22 +0200

    Added multi-threading and image resizing

--- scripts/image_downloader.py
+++ scripts/image_downloader.py
@@ -9,6 +9,10 @@ from enum import Enum
 import requests
 import numpy as np
 import pickle
+import io
+import PIL.Image as Image
+import PIL.ImageOps as ImageOps
+import threading
 
 class Tags(Enum):
     NAME = 0
@@ -17,6 +21,44 @@ class Tags(Enum):
     IMG = 3
     SUB_ID = 4
     SUB = 5
+
+def download_file_images(file, data, labels, lock: threading.RLock, image_size):
+    global collected_samples
+    csv_filename = DATASET_DIR + file
+    with open(csv_filename, 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        #read each line
+        for index, row in enumerate(reader):
+            #take one in ten
+            if index % (100 / percentage2download) == 0:
+                date = row[Tags.DATE.value]
+                #check date
+                if not date or int(date.split("-")[0]) > date_accepted:
+                    thumb_url = row[Tags.THUMBNAIL.value]
+                    
+                    answer = requests.get(thumb_url, headers=headers) #download thumbnail
+                    if answer.status_code != 404:
+                        image = answer.content
+                        #resize image
+                        image = Image.open(io.BytesIO(image))
+                        image = ImageOps.pad(image, image_size)
+                        image = np.array(image)
+                        #store resized image
+                        with lock:
+                            data.append(image)
+                            labels.append(categories[GET_CATEGORY(file)])
+                            collected_samples += 1
+
+                            #check if collected enough samples
+                            if collected_samples % samples4file == 0:
+                                with open(FINAL_DATASET_PATH(), "w") as data_file:
+                                    dict2write = {"data": np.array(data), "labels": np.array(labels)}
+                                    pickle.dump(dict2write, data_file, protocol=pickle.HIGHEST_PROTOCOL)
+                                data.clear()
+                                labels.clear()
+                            if collected_samples % 100 == 0:
+                                print(f'Collected {collected_samples} samples')
+
 
 GET_CATEGORY = lambda x: x.split("_")[0] #get the category from the filename
 DATASET_DIR = "./dataset/"
@@ -32,7 +74,7 @@ percentage2download = 10 #10%
 samples4file = 10000 #how many samples to store per file
 final_filename = "dataset"
 date_accepted = 2010 #date from which to collect samples
-
+IMAGE_SIZE = (50, 50) #the size of the final image
 
 collected_samples = 0
 FINAL_DATASET_PATH = lambda: DATASET_DIR + final_filename + int(collected_samples / samples4file) #get dataset path
@@ -44,39 +86,19 @@ for file in all_files:
     if not GET_CATEGORY(file) in categories:
         categories[GET_CATEGORY(file)] = len(categories)
 
-data = np.empty(0, dtype=object) #todo: remove dtype=object once resize has been implemented
-labels = np.empty(0)
+data = []
+labels = []
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0'} #needed, otherwise the request hangs
 #todo: is there a workaround??
 
+workers = []
+
 #files are in the dataset folder
-for file in csv_files:
-    print("Parsing " + file)
-    csv_filename = DATASET_DIR + file
+for file in reversed(csv_files):
+    worker = threading.Thread(target=download_file_images, args=(file, data, labels, threading.RLock(), IMAGE_SIZE, ))
+    worker.start()
+    workers.append(worker)
 
-    with open(csv_filename, 'r', newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        #read each line
-        for index, row in enumerate(reader):
-            #take one in ten
-            if index % (100 / percentage2download) == 0:
-                date = row[Tags.DATE.value]
-                #check date
-                if not date or int(date.split("-")[0]) > date_accepted:
-                    thumb_url = row[Tags.THUMBNAIL.value]
-                    
-                    answer = requests.get(thumb_url, headers=headers) #download thumbnail
-                    if answer.status_code != 404:
-                        image = answer.content
-                        #todo: resize image
-
-                        #store resized image
-                        data = np.append(data, image) #todo: check they append in the right axis
-                        labels = np.append(labels, GET_CATEGORY(file))
-                        collected_samples += 1
-
-                        #check if collected enough samples
-                        if collected_samples % samples4file == 0:
-                            with open(FINAL_DATASET_PATH(), "w") as data_file:
-                                dict2write = {"data": data, "labels": labels}
-                                pickle.dump(dict2write, data_file, protocol=pickle.HIGHEST_PROTOCOL)
+for thread in workers:
+    thread.join()
+    
