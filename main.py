0e69f39000cbcb331c5196c7a5a4bc15ad880925
YinengWang <77743367+YinengWang@users.noreply.github.com>
2021-05-14 17:50:01 +0200

    Reimplement dataset (#10)
    * Add configs in training
    
    * Add config: use symmetric noise
    
    * Reimplement a fast dataloader with noisy labels
    
    * Fix a bug: noise generator isn't enabled
    
    * Store the main tensors on RAM
    
    * Remove extra parameter in load_cifar100_dataset
    
    * Add test case for noise correctness
    
    * Create tests folder
    
    * Remove noise.py related function from main.py
    
    * Fix a bug: indices does not reflect true ones if shuffling is enabled
    
    * get_indices_in_batch returns correctly when shuffling is disabled
    
    * Create results folder if not exists

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ import torch
 import torch.optim as optim
 from torch.cuda.amp import GradScaler, autocast
 from tqdm import tqdm
+import numpy as np
 
 from loss import ELR_loss
 from model import ResNet18
 from model import ResNet34
-from noise import Noise
 import datasets
 
+import os.path
 from pathlib import Path
 import wandb
+import csv
 
 
 # set global env variable
@@ -29,9 +31,6 @@ def train(model, criterion, optimizer, n_epochs, train_loader, test_loader=None,
     # tell wandb to watch what the model gets up to: gradients, weights, and more!
     wandb.watch(model, criterion, log='all', log_freq=100)
 
-    train_noise_generator = Noise(train_loader, noise_rate=config.noise_rate)
-    test_noise_generator = Noise(test_loader, noise_rate=config.noise_rate) if test_loader is not None else None
-
     train_loss_per_epoch = []
     test_loss_per_epoch = []
     correct_per_epoch = []
@@ -40,17 +39,14 @@ def train(model, criterion, optimizer, n_epochs, train_loader, test_loader=None,
 
     example_ct = 0  # number of examples seen
     batch_ct = 0
-    n = len(train_loader.dataset)
     test_criterion = torch.nn.CrossEntropyLoss()
 
     for epoch in tqdm(range(n_epochs)):
         # activate train mode
         model.train()
         train_loss = 0
-        for batch_idx, (inputs, targets) in enumerate(train_loader):
-            targets_with_noise = train_noise_generator.symmetric_noise(targets, batch_idx)
-            # to(device) copies data from CPU to GPU
-            inputs, targets = inputs.to(device), targets_with_noise.to(device)
+        for batch_idx, (inputs, targets, original_targets) in enumerate(train_loader):
+            inputs, targets, original_targets = inputs.to(device), targets.to(device), original_targets.to(device)
             optimizer.zero_grad()
 
             if config.enable_amp:
@@ -59,7 +55,7 @@ def train(model, criterion, optimizer, n_epochs, train_loader, test_loader=None,
                     outputs = model(inputs)
 
                     if config['use_ELR']:
-                        inds = train_noise_generator.get_indices_in_batch(batch_idx)
+                        inds = train_loader.get_indices_in_batch(batch_idx)
                         loss = criterion(inds, outputs, targets)
                     else:
                         loss = criterion(outputs, targets)
@@ -71,7 +67,7 @@ def train(model, criterion, optimizer, n_epochs, train_loader, test_loader=None,
                 outputs = model(inputs)
 
                 if config['use_ELR']:
-                    inds = train_noise_generator.get_indices_in_batch(batch_idx)
+                    inds = train_loader.get_indices_in_batch(batch_idx)
                     loss = criterion(inds, outputs, targets)
                 else:
                     loss = criterion(outputs, targets)
@@ -91,30 +87,27 @@ def train(model, criterion, optimizer, n_epochs, train_loader, test_loader=None,
             wandb.log({"epoch": epoch, "loss": loss_batch}, step=batch_ct)
             # print(f"Loss after " + str(example_ct).zfill(5) + f" examples: {loss_for_wand_b:.3f}")
 
-        train_loss_per_epoch.append(train_loss / n)
+        train_loss_per_epoch.append(train_loss / train_loader.dataset_len)
 
         if test_loader is not None:
             model.eval()
             test_loss = 0
             with torch.no_grad():
                 correct, incorrect, memorized, total = 0, 0, 0, 0
-                with torch.no_grad():
-                    for batch_idx, (inputs, targets) in enumerate(test_loader):
-                        original_targets = targets.to(device)
-                        targets_with_noise = test_noise_generator.symmetric_noise(targets, batch_idx)
-                        inputs, targets = inputs.to(device), targets_with_noise.to(device)
-                        outputs = model(inputs)
-                        loss = test_criterion(outputs, targets)
+                for batch_idx, (inputs, targets, original_targets) in enumerate(test_loader):
+                    inputs, targets, original_targets = inputs.to(device), targets.to(device), original_targets.to(device)
+                    outputs = model(inputs)
+                    loss = test_criterion(outputs, targets)
 
-                        _, predicted = outputs.max(1)
-                        total += targets.size(0)
-                        correct_idx = predicted.eq(original_targets)
-                        memorized_idx = ((predicted != original_targets) & (predicted == targets))
-                        incorrect_idx = ((predicted != original_targets) & (predicted != targets))
-                        correct += correct_idx.sum().item()
-                        memorized += memorized_idx.sum().item()
-                        incorrect += incorrect_idx.sum().item()
-                        test_loss += loss.item() * targets.size(0)
+                    _, predicted = outputs.max(1)
+                    total += targets.size(0)
+                    correct_idx = predicted.eq(original_targets)
+                    memorized_idx = ((predicted != original_targets) & (predicted == targets))
+                    incorrect_idx = ((predicted != original_targets) & (predicted != targets))
+                    correct += correct_idx.sum().item()
+                    memorized += memorized_idx.sum().item()
+                    incorrect += incorrect_idx.sum().item()
+                    test_loss += loss.item() * targets.size(0)
 
                 test_loss_per_epoch.append(test_loss / total)
                 correct_per_epoch.append(correct / total)
@@ -133,24 +126,48 @@ def train(model, criterion, optimizer, n_epochs, train_loader, test_loader=None,
     return train_loss_per_epoch, test_loss_per_epoch, correct_per_epoch, memorized_per_epoch, incorrect_per_epoch
 
 
-def plot_learning_curve_and_acc(train_cost, test_cost, test_correct, test_memorized, test_incorrect, title):
+def plot_learning_curve_and_acc(results, title, path_prefix):
+    Path("./results").mkdir(parents=True, exist_ok=True)
+    train_cost, test_cost, test_correct, test_memorized, test_incorrect = results
+    epochs = np.arange(1, len(train_cost) + 1)
     # plot learning curve
-    plt.plot(train_cost)
-    plt.plot(test_cost)
+    plt.plot(epochs, train_cost)
+    plt.plot(epochs, test_cost)
     plt.title(title)
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.legend(['Train', 'Test'])
+    plt.savefig(path_prefix + '_loss.pdf')
     plt.show()
 
-    plt.plot(test_correct)
-    plt.plot(test_memorized)
-    plt.plot(test_incorrect)
+    # plot fraction of correct, incorrect, memorized samples
+    plt.plot(epochs, test_correct)
+    plt.plot(epochs, test_memorized)
+    plt.plot(epochs, test_incorrect)
     plt.title(title)
     plt.xlabel('Epoch')
     plt.ylabel('Fraction of examples')
     plt.legend(['Correct', 'Memorized', 'Incorrect'])
+    plt.savefig(path_prefix + '_acc.pdf')
     plt.show()
+
+
+def record_results(filepath, dataset, noise_rate, is_symmetric_noise, enable_amp, results):
+    fieldnames = ['dataset', 'noise_rate', 'is_symmetric_noise', 'enable_amp',
+                  'train_loss', 'test_loss', 'correct', 'memorized', 'incorrect']
+    if not os.path.exists(filepath):
+        with open(filepath, 'w') as f:
+            writer = csv.DictWriter(f, fieldnames)
+            writer.writeheader()
+    with open(filepath, 'a') as f:
+        writer = csv.DictWriter(f, fieldnames)
+        train_loss, test_loss, correct, memorized, incorrect = results
+        writer.writerow({
+            'dataset': dataset, 'noise_rate': noise_rate, 'is_symmetric_noise': is_symmetric_noise,
+            'enable_amp': enable_amp,
+            'train_loss': train_loss[-1], 'test_loss': test_loss[-1],
+            'correct': correct[-1], 'memorized': memorized[-1], 'incorrect': incorrect[-1]
+        })
 
 
 def model_pipeline(config, trainer_config, loadExistingWeights=False):
@@ -170,10 +187,12 @@ def model_pipeline(config, trainer_config, loadExistingWeights=False):
         print('==> Preparing data..')
         if config.dataset_name == 'CIFAR10':
             output_features = 10
-            train_loader, test_loader = datasets.load_cifar10_dataset(batch_size=config.batch_size)
+            train_loader, test_loader = datasets.load_cifar10_dataset(batch_size=config.batch_size,
+                                                                      noise_rate=config.noise_rate)
         elif config.dataset_name == 'CIFAR100':
             output_features = 100
-            train_loader, test_loader = datasets.load_cifar100_dataset(batch_size=config.batch_size)
+            train_loader, test_loader = datasets.load_cifar100_dataset(batch_size=config.batch_size,
+                                                                       noise_rate=config.noise_rate)
         elif config.dataset_name == 'CDON':
             raise NotImplementedError
         else:
@@ -182,7 +201,7 @@ def model_pipeline(config, trainer_config, loadExistingWeights=False):
         """training algorithm"""
         if config['use_ELR']:
             print('--Using ELR--')
-            criterion = trainer_config['criterion'](len(train_loader.dataset), n_classes=output_features, **trainer_config['criterion_params'])
+            criterion = trainer_config['criterion'](train_loader.dataset_len, n_classes=output_features, **trainer_config['criterion_params'])
         else:
             print('--Using CE loss--')
             criterion = trainer_config['criterion'](**trainer_config['criterion_params'])
@@ -192,30 +211,29 @@ def model_pipeline(config, trainer_config, loadExistingWeights=False):
         scheduler = trainer_config['scheduler'](optimizer, **trainer_config['scheduler_params'])
 
         """train model"""
-        train_loss_per_epoch, test_loss_per_epoch, correct_per_epoch, memorized_per_epoch, incorrect_per_epoch = train(
+        results = train(
             model, criterion, optimizer, n_epochs=config.n_epochs, train_loader=train_loader, test_loader=test_loader,
             scheduler=scheduler, config=config)
 
         """Plot learning curve and accuracy"""
-        print(f'acc={correct_per_epoch[-1]}, memorized={memorized_per_epoch[-1]}')
         plot_title = f'{config.dataset_name}, noise_level={config.noise_rate}'
-        plot_learning_curve_and_acc(train_loss_per_epoch, test_loss_per_epoch,
-                                    correct_per_epoch, memorized_per_epoch, incorrect_per_epoch, plot_title)
+        plot_learning_curve_and_acc(results, plot_title, config.plot_path)
         Path("./models").mkdir(parents=True, exist_ok=True)
         torch.save(model.state_dict(), config.model_path)
 
 
 def main():
-
     wandb.login()
 
     config = dict(
-        n_epochs=5,
+        n_epochs=120,
         batch_size=128,
         classes=10,
         noise_rate=0.2,
+        is_symmetric_noise=True,
         dataset_name='CIFAR10',  # opt: 'CIFAR10', 'CIFAR100', 'CDON' (not implemented)
-        model_path='./models/CIFAR10_noise_level_20.mdl',
+        model_path='./models/CIFAR10_20.mdl',
+        plot_path='./results/CIFAR10_20',
         learning_rate=0.02,
         momentum=0.9,
         weight_decay=1e-3,
